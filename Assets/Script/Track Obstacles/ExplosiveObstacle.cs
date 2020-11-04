103c26599a2f75ad484dfcf1ed153e6079422962
Marko Tot <acw549@qmul.ac.uk>
2020-11-04 17:53:12 +0000

    Added Explosive obstacle

---
+++ Assets/Script/Track Obstacles/ExplosiveObstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveObstacle : MonoBehaviour, IObstacle
+{
+    public void Activate()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
