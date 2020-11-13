2ad2f349f4752a49f43d59289dc300a8f1c3667a
Marko Tot <acw549@qmul.ac.uk>
2020-11-13 13:54:13 +0000

    Playtest friday ready

--- Assets/Script/Weapons/BulletScript.cs
+++ Assets/Script/Weapons/BulletScript.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public Rigidbody rigidBody;
+
+    protected List<Collider> chariotColliders = new List<Collider>();
     // Start is called before the first frame update
     public float bulletSpeed = 50f;
     public float aliveTime = 5f;
 
     public void Start()
     {
-       StartCoroutine(DestroySelf(aliveTime));
+        foreach (PlayerController pc in LevelController.Instance.AIControllers)
+        {
+            chariotColliders.Add(pc.CollectionCollider);
+        }
+        StartCoroutine(DestroySelf(aliveTime));
     }
     public void SetVelocity(Vector3 direction, Vector3 initialVelocity)
     {
@@ -22,12 +28,13 @@ public class BulletScript : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
 
-        if (other.CompareTag("enemy"))
+        if (chariotColliders.Contains(other))
         {
-            other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000f, other.transform.position,5f,100f);
+            other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000f, other.transform.position, 5f, 100f);
             Destroy(gameObject);
             Destroy(other.transform.parent.gameObject, 1f);
         }
+
         
     }
 
